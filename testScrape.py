a8c8b34afa226c71cc0ddb1401bf73423842f55c
Jonathan Burge <jburge@u.northwestern.edu>
2016-09-28 15:17:29 -0500

    multipage scrape

--- testScrape.py
+++ testScrape.py
@@ -2,60 +2,63 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
-initialyear = 1968;
-finalyear = 1992;
-
-url = "http://www.informs-sim.org/wsc90papers/prog90sim.html"
-titles = list()
-authors = list(list())
-r = requests.get(url)
-
-data = r.text
-
-soup = BeautifulSoup(data)
-titlesSpan = soup.findAll("span", class_ = "paperTitle")
-authorsSpan = soup.findAll("span", class_ = "author")
-selected = soup.findAll(['span', 'h2','h3'])
-print(len(selected))
+initialyear = 1968
+#finalyear = 1992;
+finalyear = 1970
 items = list(list())
 category = ''
 subcategory = ''
 
+titles = list()
+authors = list(list())
+url = ["http://www.informs-sim.org/wsc", "papers/prog", "sim.html"]
 
-for line in selected:
-		try:
-			found = re.search('<h2>(.*?)</h2>', str(line)).group(1)
-			category = found
-			subcategory = ''
-		except AttributeError:
-			found =''
+for year in range(initialyear, finalyear):
+	url_year = str(year % 100)
+	url_to_scrape = url[0] + url_year + url[1] + url_year + url[2]
+	r = requests.get(url_to_scrape)
+	data = r.text
 
-		try:
-			found = re.search('<h3>(.*?)</h3>', str(line)).group(1)
-			subcategory = found
-		except AttributeError:
-			found = ''
-#	if 'class=\"author\"' in line:
-#		print("Found author")
-		try:
-			found = re.search('"author">(.*?)</span>', str(line)).group(1)
-			found = found.replace(' and ', ',')
-			found = found.split(',')
-			found = filter(None, found)
-#			authors.append(found)
-			item  = [category, subcategory, title, found]
-			items.append(item)
-		except AttributeError:
-			found = ''	
-#	elif 'class=\"paperTitle\"' in line:
-#		print("Found title")
-		try:
-			found = re.search('blank">(.*?)</a', str(line)).group(1)
-#			titles.append(found)
-			title = found
-		except AttributeError:
-			found = ''
+	soup = BeautifulSoup(data)
+	titlesSpan = soup.findAll("span", class_ = "paperTitle")
+	authorsSpan = soup.findAll("span", class_ = "author")
+	selected = soup.findAll(['span', 'h2','h3'])
+	print(len(selected))
 
-print(len(titles))
-for i in range(1, len(items)):
-	print(str(items[i]))
+	for line in selected:
+			try:
+				found = re.search('<h2>(.*?)</h2>', str(line)).group(1)
+				category = found
+				subcategory = ''
+			except AttributeError:
+				found =''
+
+			try:
+				found = re.search('<h3>(.*?)</h3>', str(line)).group(1)
+				subcategory = found
+			except AttributeError:
+				found = ''
+	#	if 'class=\"author\"' in line:
+	#		print("Found author")
+			try:
+				found = re.search('"author">(.*?)</span>', str(line)).group(1)
+				found = found.replace(' and ', ',')
+				found = found.split(',')
+				found = filter(None, found)
+	#			authors.append(found)
+				item  = [category, subcategory, title, year, found]
+				items.append(item)
+			except AttributeError:
+				found = ''	
+	#	elif 'class=\"paperTitle\"' in line:
+	#		print("Found title")
+			try:
+				found = re.search('blank">(.*?)</a', str(line)).group(1)
+	#			titles.append(found)
+				title = found
+			except AttributeError:
+				found = ''
+
+	print(len(titles))
+	for i in range(1, len(items)):
+		print(str(items[i]))
