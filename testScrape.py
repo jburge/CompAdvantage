bb5c65a859c163852f1282c177d85ba1acaf0610
Jonathan Burge <jburge@u.northwestern.edu>
2016-09-28 13:39:58 -0500

    somewhat function scrapper for single page created

--- testScrape.py
+++ testScrape.py
@@ -15,25 +15,44 @@ titlesSpan = soup.findAll("span", class_ = "paperTitle")
 authorsSpan = soup.findAll("span", class_ = "author")
 selected = soup.findAll(['span', 'h2','h3'])
 print(len(selected))
-##, class_ = ['paperTitle', 'author'])
-print(selected[1])
+items = list(list())
+category = ''
+subcategory = ''
+
+print(selected[100])
 for line in selected:
-	if "class=\"author\"" in line:
-		print("Found author")
 		try:
-			found = re.search('"author">(.*?</span>', str(line)).group(1)
+			found = re.search('<h2>(.*?)</h2>', str(line)).group(1)
+			category = found
+		except AttributeError:
+			found =''
+
+		try:
+			found = re.search('<h3>(.*?)</h3>', str(line)).group(1)
+			subcategory = found
+		except AttributeError:
+			found = ''
+#	if 'class=\"author\"' in line:
+#		print("Found author")
+		try:
+			found = re.search('"author">(.*?)</span>', str(line)).group(1)
+			found = found.replace(' and ', ',')
 			found = found.split(',')
-			authors.append(found)
+			found = filter(None, found)
+#			authors.append(found)
+			item  = [category, subcategory, title, found]
+			items.append(item)
 		except AttributeError:
 			found = ''	
-	elif "class=\"paperTitle\"" in line:
-		print("Found title")
+#	elif 'class=\"paperTitle\"' in line:
+#		print("Found title")
 		try:
-			found = re.search('blank">(.*?)</a', str(i)).group(1)
-			titles.append(found)
+			found = re.search('blank">(.*?)</a', str(line)).group(1)
+#			titles.append(found)
+			title = found
 		except AttributeError:
 			found = ''
 
 print(len(titles))
-for i in range(1, len(titles)):
-	print(title[i] + str(author[i]))
+for i in range(1, len(items)):
+	print(str(items[i]))
