d195689f111e50d10fcef5a9abd057c9415440b4
Jonathan Burge <jburge@u.northwestern.edu>
2016-09-28 13:12:52 -0500

    work on testscrape script

--- testScrape.py
+++ testScrape.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
+
 url = "http://www.informs-sim.org/wsc90papers/prog90sim.html"
 titles = list()
 authors = list(list())
@@ -12,21 +13,27 @@ data = r.text
 soup = BeautifulSoup(data)
 titlesSpan = soup.findAll("span", class_ = "paperTitle")
 authorsSpan = soup.findAll("span", class_ = "author")
-for i in titlesSpan:
-	try:
-		found = re.search('blank">(.*?)</a', str(i)).group(1)
-		titles.append(found)
-	except AttributeError:
-		found = ''
-for i in authorsSpan:
-	try:
-		found = re.search('"author">(.*?)</span>', str(i)).group(1)
-		found = found.split(',')
-		authors.append(found)
-		for j in found:
-			print j
-	except AttributeError:
-		found = ''
-for i in range(1, 10):
-	print(titles[i] + " by " + str(authors[i]))
+selected = soup.findAll(['span', 'h2','h3'])
+print(len(selected))
+##, class_ = ['paperTitle', 'author'])
+print(selected[1])
+for line in selected:
+	if "class=\"author\"" in line:
+		print("Found author")
+		try:
+			found = re.search('"author">(.*?</span>', str(line)).group(1)
+			found = found.split(',')
+			authors.append(found)
+		except AttributeError:
+			found = ''	
+	elif "class=\"paperTitle\"" in line:
+		print("Found title")
+		try:
+			found = re.search('blank">(.*?)</a', str(i)).group(1)
+			titles.append(found)
+		except AttributeError:
+			found = ''
 
+print(len(titles))
+for i in range(1, len(titles)):
+	print(title[i] + str(author[i]))
