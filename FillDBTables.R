b594602ee9e24645a733d5fa220f8b651e6ead7e
Jonathan Burge <jburge@u.northwestern.edu>
2016-10-03 16:10:39 -0500

    update r code

--- FillDBTables.R
+++ FillDBTables.R
@@ -1,5 +1,8 @@
 dat <- read.csv("~/z/CompAnalysis/CompAdvantage/test.csv",header=FALSE, stringsAsFactors = FALSE)
 
+rs = dbSendQuery(cnx, "SELECT * FROM jburge.winsim_article;");
+article = dbFetch(rs)
+
 cats = unique(dat$V1)
 subcats = unique(dat$V2)
 titles = dat$V3
@@ -10,21 +13,23 @@ articles = as.data.frame(unique(cbind(titles, years)), stringsAsFactors = FALSE)
 
 for(entry in cats)
   {
-    sql = paste("INSERT INTO scratch.jburge.winsim_category (name) VALUES ($$", entry,"$$)")
+    sql = paste("INSERT INTO scratch.jburge.winsim_category (name) VALUES ($$", entry,"$$)", sep = "")
     dbGetQuery(cnx, sql)
 }
 for(entry in subcats)
 {
-  sql = paste("INSERT INTO scratch.jburge.winsim_subcategory (name) VALUES ($$", entry,"$$)")
+  sql = paste("INSERT INTO scratch.jburge.winsim_subcategory (name) VALUES ($$", entry,"$$)", sep = "")
   dbGetQuery(cnx, sql)
 }
 for(entry in authors)
 {
-  sql = paste("INSERT INTO scratch.jburge.winsim_author (name) VALUES ($$", entry,"$$)")
+  sql = paste("INSERT INTO scratch.jburge.winsim_author (name) VALUES ($$", entry,"$$)", sep = "")
   dbGetQuery(cnx, sql)
 }
 for(i in 1:nrow(articles))
 {
-  sql = paste("INSERT INTO scratch.jburge.winsim_article (title, year) VALUES ($$", articles[i,1], "$$,$$", articles[i,2], "$$)")
+  sql = paste("INSERT INTO scratch.jburge.winsim_article (title, year) VALUES ($$", articles[i,1], "$$,$$", articles[i,2], "$$)", sep = "")
   dbGetQuery(cnx, sql)
-  }
+}
+
+mapping = merge(x = article_author, y = author, by = "name")
