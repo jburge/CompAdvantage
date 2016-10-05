0f88bce57d34524ed1e7a45f73052eaca106e9eb
Jonathan Burge <jburge@u.northwestern.edu>
2016-10-05 11:12:43 -0500

    updated code to insert into tables

--- RCode/FillDBTables.R
+++ RCode/FillDBTables.R
@@ -1,12 +1,10 @@
-dat <- read.csv("~/z/CompAnalysis/CompAdvantage/test.csv",header=FALSE, stringsAsFactors = FALSE)
+library(dplyr)
+
+dat <- read.csv("~/z/CompAnalysis/CompAdvantage/PythonScraping/scrapedData.csv",header=FALSE, stringsAsFactors = FALSE)
 
 cats = unique(dat$V1)
 subcats = unique(dat$V2)
-titles = dat$V3
-years = dat$V4
 authors = unique(dat$V5)
-
-articles = as.data.frame(unique(cbind(titles, years)), stringsAsFactors = FALSE)
 
 for(entry in cats)
   {
@@ -23,9 +21,30 @@ for(entry in authors)
   sql = paste("INSERT INTO scratch.jburge.winsim_author (name) VALUES ($$", entry,"$$)", sep = "")
   dbGetQuery(cnx, sql)
 }
+
+
+articles = dat[,(!names(dat) %in% "V5")]
+colnames(articles) = c("category", "subcategory", "title", "year")
+articles = unique(articles)
+
+rs = dbSendQuery(cnx, "SELECT * FROM jburge.winsim_subcategory;");
+subcat = dbFetch(rs)
+colnames(subcat) = c("subcategory_id", "subcategory")
+
+rs = dbSendQuery(cnx, "SELECT * FROM jburge.winsim_category;");
+cat = dbFetch(rs)
+colnames(cat) = c("category_id", "category")
+
+
+articles = merge(articles, subcat, by = "subcategory")
+articles = merge(articles, cat, by = "category")
+articlesFull = articles
+articles = articles[, (!names(articles) %in% c("category", "subcategory"))]
+
 for(i in 1:nrow(articles))
 {
-  sql = paste("INSERT INTO scratch.jburge.winsim_article (title, year) VALUES ($$", articles[i,1], "$$,$$", articles[i,2], "$$)", sep = "")
+  sql = paste("INSERT INTO scratch.jburge.winsim_article (title, year, subcategory_id, category_id) VALUES ($$",
+              articles[i,1], "$$,$$", articles[i,2], "$$,$$", articles[i,3], "$$,$$", articles[i,4], "$$)", sep = "")
   dbGetQuery(cnx, sql)
 }
 
@@ -35,48 +54,39 @@ article = dbFetch(rs)
 rs = dbSendQuery(cnx, "SELECT * FROM jburge.winsim_author;");
 author = dbFetch(rs)
 
-rs = dbSendQuery(cnx, "SELECT * FROM jburge.winsim_subcategory;");
-subcat = dbFetch(rs)
+articlesFull = merge(articlesFull, subcat, by = "subcategory")
+articlesFull = merge(articlesFull, cat, by = "category")
+articlesFull = articlesFull[,(!names(articlesFull) %in% c("category_id.x", "subcategory_id.x"))]
+colnames(articlesFull)[5] = "subcategory_id"
+colnames(articlesFull)[6] = "category_id"
 
-rs = dbSendQuery(cnx, "SELECT * FROM jburge.winsim_category;");
-cat = dbFetch(rs)
+articlesFull = merge(articlesFull, article, by = c("title", "year", "category_id", "subcategory_id"))
+articlesFull = articlesFull[,(!names(articlesFull) %in% c("category_id", "subcategory_id"))]
 
-aa = as.data.frame(cbind(dat$V3,dat$V4, dat$V5), stringsAsFactors = FALSE, row.names = FALSE)
-colnames(aa) = c("title", "year", "name")
-mapping = merge(x = aa, y = author, by = "name")
-mapping = merge(x = mapping, y = article, by = "title")
-mapping = mapping[, (!names(mapping) %in% c("title", "name", "category_id", "subcategory_id"))]
-mapping = unique(mapping)
-mapping = mapping[, (!names(mapping) %in% "year")]
-for(i in 1:nrow(mapping))
+colnames(dat) = c("category", "subcategory", "title", "year", "author")
+aamapping = merge(articlesFull, dat, c("title", "year", "subcategory", "category"))
+aamapping = aamapping[,(names(aamapping) %in% c("author", "article_id"))]
+aamapping = inner_join(aamapping, author, by = c("author" = "name"))
+aamapping = aamapping[,(!names(aamapping) %in% "author")]
+
+
+for(i in 1:nrow(aamapping))
 {
-  sql = paste("INSERT INTO scratch.jburge.winsim_author_article (author_id, article_id) VALUES ($$", mapping[i,1], "$$,$$", mapping[i,2], "$$)", sep = "")
+  sql = paste("INSERT INTO scratch.jburge.winsim_author_article (article_id, author_id) VALUES ($$", aamapping[i,1], "$$,$$", aamapping[i,2], "$$)", sep = "")
   dbGetQuery(cnx, sql)
 }
 
-acs = as.data.frame(cbind(dat$V1, dat$V2, dat$V3, dat$V4), stringsAsFactors = FALSE, 
-                    row.names = FALSE)
-colnames(acs) =  c("category", "subcategory", "title", "year")
-mapping = join(acs, article, by = c("year" = "year", "title" = "title"))
-mapping = unique(mapping)
-mapping = mapping [, (!names(mapping) %in% c("category_id", "subcategory_id"))]
-mapping = merge(mapping, cat, by.x = "category", by.y = "name")
-mapping = merge( mapping, subcat, by.x = "subcategory", by.y = "name")
-mapping = mapping[, (names(mapping) %in% c("article_id", "category_id.x", "subcategory_id"))]
-colnames(mapping) = c("article_id", "category_id", "subcategory_id")
+csmapping = as.data.frame(cbind(dat$category, dat$subcategory), stringsAsFactors = FALSE, head(FALSE))
+colnames(csmapping) = c("category", "subcategory")
+csmapping = inner_join(csmapping, cat, by = "category")
+csmapping = inner_join(csmapping, subcat, by = "subcategory")
+csmapping = csmapping[, (!names(csmapping) %in% c("category", "subcategory"))]
+csmapping = unique(csmapping)
 
-for(i in 1:nrow(mapping))
+
+for(i in 1:nrow(csmapping))
 {
-  sql = paste("UPDATE scratch.jburge.winsim_article SET category_id = $$", mapping[i,2],
-"$$,subcategory_id = $$", mapping[i,3], "$$ WHERE article_id = $$", mapping[i,1], "$$;", sep = "")
-  dbGetQuery(cnx, sql)
-}
-
-mapping = unique(mapping[, (!names(mapping) %in% "article_id")])
-
-
-for(i in 1:nrow(mapping))
-{
-  sql = paste("INSERT INTO scratch.jburge.winsim_category_subcategory (category_id, subcategory_id) VALUES ($$", mapping[i,1], "$$,$$", mapping[i,2], "$$)", sep = "")
+  sql = paste("INSERT INTO scratch.jburge.winsim_category_subcategory (category_id, subcategory_id) VALUES ($$",
+              csmapping[i,1], "$$,$$", csmapping[i,2], "$$)", sep = "")
   dbGetQuery(cnx, sql)
 }
