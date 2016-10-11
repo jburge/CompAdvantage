52863c3daef57c7640827ea8bc02212c155d70ae
Jonathan Burge <jburge@u.northwestern.edu>
2016-10-10 20:12:16 -0500

    tuning MCL parameters

--- InitialExploration/AuthorClustering/InitialExploration.R
+++ InitialExploration/AuthorClustering/InitialExploration.R
@@ -53,60 +53,74 @@ cleanmat = authormat[test ,test]
 #normalize
   #for each column normalize to sum to 1
 normmat = cleanmat %*% diag(1/colSums(cleanmat))
+#e_set = c(2, 2.4, 2.8, 3.2, 3.6, 4)
+#r_set = c(2, 2.4, 2.8, 3.2, 3.6, 4)
+# best seem to be 4, 2.4 (have low dupe count and high performance)
 e = 2.4
-r = 2.8
-toleranceForConvergence = .0001
-m = normmat
-while(TRUE)
-{
-  #expand
-  expand = m %^% e
+r = 1.4
+toleranceForConvergence = .00001
+output = list()
+#for(e in e_set)
+#{
+  #for(r in r_set)
+  #{
+  m = normmat
+  while(TRUE)
+  {
+    #expand
+    expand = m %^% e
+    
+    #inflate
+    num = expand ^ r
+    inflate = num %*% diag(1/colSums(num))
+    maxdivergence = max(abs(m - inflate))
+    m = inflate
+    if(maxdivergence < toleranceForConvergence)
+      break;
+    print(maxdivergence)
+  }
   
-  #inflate
-  num = expand ^ r
-  inflate = num %*% diag(1/colSums(num))
-  maxdivergence = max(abs(m - inflate))
-  m = inflate
-  if(maxdivergence < toleranceForConvergence)
-    break;
-  print(maxdivergence)
-}
-
-### draw conclusions from the resulting matrix
-nonzero = as.data.frame(cbind(row(m)[m!=0],col(m)[m!=0]))
-nonzero = nonzero[order(nonzero$V1),]
-
-
-clusters = vector("list", nrow(author))
-for(i in 1:nrow(author))
-{
-  temp = nonzero[nonzero$V1 == i,2]
-  clusters[[i]] = sort(temp)
-}
-t = table(sapply(clusters, length, simplify = TRUE))
-
-clusters = unique(clusters)
-clusters = clusters[order(sapply(clusters,length),decreasing=T)]
-c = clusters[lengths(clusters) > 1]
-t = table(sapply(c, length, simplify = TRUE))
-
-#determine effectiveness of clusters
-getEdgesInCluster = function(x){
-  s = sum(edges[(edges$r1 %in% x) & (edges$r2 %in% x), 3])
-  return(s)
-}
-getEdgesTouchingCluster = function(x){
-  s = sum(edges[(edges$r1 %in% x) | (edges$r2 %in% x), 3])
-  return(s)
-}
-clusterEdges = lapply(c[], getEdgesInCluster)
-clusterFringe = lapply(c[], getEdgesTouchingCluster)
-clusterEdges = unlist(x = clusterEdges)
-clusterFringe = unlist(clusterFringe)
-t
-table(clusterFringe)
-table(clusterEdges)
-table(clusterFringe - clusterEdges)
-percentInclusion = clusterEdges / clusterFringe
-percentInclusion = percentInclusion[!percentInclusion %in% c(0, 1)]
-percentInclusion
+  ### draw conclusions from the resulting matrix
+  nonzero = as.data.frame(cbind(row(m)[m!=0],col(m)[m!=0]))
+  nonzero = nonzero[order(nonzero$V1),]
+  
+  
+  clusters = vector("list", nrow(author))
+  for(i in 1:nrow(author))
+  {
+    temp = nonzero[nonzero$V1 == i,2]
+    clusters[[i]] = sort(temp)
+  }
+  t = table(sapply(clusters, length, simplify = TRUE))
+  
+  clusters = unique(clusters)
+  clusters = clusters[order(sapply(clusters,length),decreasing=T)]
+  c = clusters[lengths(clusters) > 1]
+  t = table(sapply(clusters, length, simplify = TRUE))
+  
+  #determine effectiveness of clusters
+  getEdgesInCluster = function(x){
+    s = sum(edges[(edges$r1 %in% x) & (edges$r2 %in% x), 3])
+    return(s)
+  }
+  getEdgesTouchingCluster = function(x){
+    s = sum(edges[(edges$r1 %in% x) | (edges$r2 %in% x), 3])
+    return(s)
+  }
+  clusterEdges = lapply(c[], getEdgesInCluster)
+  clusterFringe = lapply(c[], getEdgesTouchingCluster)
+  clusterEdges = unlist(x = clusterEdges)
+  clusterFringe = unlist(clusterFringe)
+  t
+  table(clusterFringe)
+  table(clusterEdges)
+  table(clusterFringe - clusterEdges)
+  percentInclusion = clusterEdges / clusterFringe
+  (overallPerformance = sum(clusterEdges) / sum(clusterFringe))
+  cluster_items = unlist(clusters)
+  dup_items = cluster_items[duplicated(cluster_items)]
+  length(dup_items)
+  author[author$row_num %in% dup_items,]
+  output = c(output, paste(e, r, overallPerformance))
+#  }
+#}
